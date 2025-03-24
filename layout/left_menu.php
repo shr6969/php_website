9657ba331f76b041c2fa16429c6c3d92f6ac2ea5
Sofiia <sofiahruba1129@gmail.com>
2025-03-24 14:15:17 +0100

    лр2 завд2

--- layout/left_menu.php
+++ layout/left_menu.php
@@ -1,6 +1,6 @@
 <nav class="sidebar">
     <ul>
         <li><a href="index.php">Головна</a></li>
-        <li><a href="about.php">Про сайт</a></li>
+        <li><a href="index.php?page=about">Про сайт</a></li>
     </ul>
 </nav>
